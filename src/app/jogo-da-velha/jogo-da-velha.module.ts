f4c38dff5ecf363044a5ec4fd981a625d7c9d57b
Raquel <raquel.costa@ttwgroup.com>
2017-08-15 12:58:21 -0300

    Implement jogoDaVelhaService

--- src/app/jogo-da-velha/jogo-da-velha.module.ts
+++ src/app/jogo-da-velha/jogo-da-velha.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { JogoDaVelhaComponent } from './jogo-da-velha.component';
-
+import { JogoDaVelhaService } from './shared';
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [JogoDaVelhaComponent]
+  declarations: [JogoDaVelhaComponent],
+  exports: [JogoDaVelhaComponent],
+  providers: [JogoDaVelhaService]
 })
 export class JogoDaVelhaModule { }
