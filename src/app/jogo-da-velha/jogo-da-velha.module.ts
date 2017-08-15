7f6da60a283904f42873db92a34fd654065cfd38
Raquel <raquel.costa@ttwgroup.com>
2017-08-15 12:49:39 -0300

    Create component jogo-da-velha

--- src/app/jogo-da-velha/jogo-da-velha.module.ts
+++ src/app/jogo-da-velha/jogo-da-velha.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { JogoDaVelhaComponent } from './jogo-da-velha.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [JogoDaVelhaComponent]
 })
 export class JogoDaVelhaModule { }
